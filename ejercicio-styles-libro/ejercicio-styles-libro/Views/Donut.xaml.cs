a3c5fe74fbf5ebf4955e0053b46167a0c29b78a3
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2017-01-16 14:15:54 +0100

    Terminar la ultima pestaña

--- ejercicio-styles-libro/ejercicio-styles-libro/Views/Donut.xaml.cs
+++ ejercicio-styles-libro/ejercicio-styles-libro/Views/Donut.xaml.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -23,6 +25,7 @@ namespace ejercicio_styles_libro
     /// </summary>
     public sealed partial class Donut : Page
     {
+        SystemNavigationManager systemNavigationManager;
         public Donut()
         {
             this.InitializeComponent();
@@ -48,6 +51,40 @@ namespace ejercicio_styles_libro
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             splitPrincipal.IsPaneOpen = !splitPrincipal.IsPaneOpen;
+        }
+        private void appointmentsDetail_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            // Mark event as handled so we don't get bounced out of the app.
+            e.Handled = true;
+            OnBackRequested();
+        }
+        private void OnBackRequested()
+        {
+            // Page above us will be our master view.
+            // Make sure we are using the "drill out" animation in this transition.
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack(new SuppressNavigationTransitionInfo());
+            }else
+            {
+                systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+            
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested -= appointmentsDetail_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested += appointmentsDetail_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
         }
     }
 }
--- ejercicio-styles-libro/ejercicio-styles-libro/Views/Schedule.xaml.cs
+++ ejercicio-styles-libro/ejercicio-styles-libro/Views/Schedule.xaml.cs
@@ -6,12 +6,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=234238
@@ -48,6 +50,35 @@ namespace ejercicio_styles_libro
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
             splitPrincipal.IsPaneOpen = !splitPrincipal.IsPaneOpen;
+        }
+
+
+        private void appointmentsDetail_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            // Mark event as handled so we don't get bounced out of the app.
+            e.Handled = true;
+            OnBackRequested();
+        }
+        private void OnBackRequested()
+        {
+            // Page above us will be our master view.
+            // Make sure we are using the "drill out" animation in this transition.
+            Frame.GoBack(new SuppressNavigationTransitionInfo());
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested -= appointmentsDetail_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
+            systemNavigationManager.BackRequested += appointmentsDetail_BackRequested;
+            systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
         }
     }
 }
